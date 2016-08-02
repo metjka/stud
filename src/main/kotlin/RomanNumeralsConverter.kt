eaa08c27cc1c88f962e64b487f83176e62d8161c
Ihor Salnikov <isalnikov@pgs-soft.com>
2016-08-02 16:41:05 +0200

    work

--- src/main/kotlin/RomanNumeralsConverter.kt
+++ src/main/kotlin/RomanNumeralsConverter.kt
@@ -1,6 +1,3 @@
-import com.sun.deploy.util.ArrayUtil
-import org.apache.commons.lang.ArrayUtils
-
 /**
  * Created by isalnikov on 8/1/2016.
  * Time 3:42 PM
@@ -20,43 +17,71 @@ class RomanNumeralsConverter : IRomanNumeralsConverter {
     private val M = 1000
 
     override fun convert(number: String): Int {
-        val uppercaseNum = number.toUpperCase().trim().toCharArray()
+        val uppercaseNum: CharArray = number.toUpperCase().trim().toCharArray()
         val count = uppercaseNum.count()
 
         when {
             count == 1 -> return basicConverter(uppercaseNum[0])
-            count > 0 -> return advancedConverter(uppercaseNum)
+            count > 1 -> return advancedConverter(uppercaseNum)
             else -> throw IllegalArgumentException("String is funny!")
         }
     }
 
     private fun advancedConverter(uppercaseNum: CharArray): Int {
-        val count = uppercaseNum.count()
-        var list = uppercaseNum
-
+        var list: Array<Char> = uppercaseNum.toTypedArray()
+        var sum: Int = 0
         while (list.isNotEmpty()) {
-            val last = list.lastIndex
-            
-            if ()
-                ArrayUtils.removeElement
+            val lastIndex = list.lastIndex
+            val last = basicConverter(list.last())
+            var dummySum: Int = last
+            var c: Int = 0
+            if (lastIndex > 3) {
+                for (i in lastIndex downTo lastIndex - 3) {
+                    if (last >= basicConverter(list[i])) {
+                        c++
+                        dummySum += basicConverter(list[i])
+                    } else if (last < basicConverter(list[i])) {
+                        break
+                    }
+                }
+            }
+            var ac = advancedSum(list.takeLast(c))
+            list = list.dropLast(c).toTypedArray()
         }
         return 1;
     }
-}
 
-
-private fun basicConverter(later: Char): Int {
-    when (later) {
-        'I' -> return I
-        'V' -> return V
-        'X' -> return X
-        'L' -> return L
-        'C' -> return C
-        'D' -> return D
-        'M' -> return M
-        else -> throw IllegalArgumentException("Number is invalid!")
+    private fun basicConverter(later: Char): Int {
+        when (later) {
+            'I' -> return I
+            'V' -> return V
+            'X' -> return X
+            'L' -> return L
+            'C' -> return C
+            'D' -> return D
+            'M' -> return M
+            else -> throw IllegalArgumentException("Number is invalid!")
+        }
     }
-}
+
+
+    private fun advancedSum(lis: List<Char>) {
+
+        val numlist: List<Int> = lis.map { it -> basicConverter(it) }
+        val maxIndex: Int? = numlist.maxBy { numlist[it] } ?: -1
+
+        var num: Int = 0
+
+        if (maxIndex == numlist.lastIndex) {
+            for (i in  0..numlist.lastIndex - 1) {
+                 numlist.get(i)
+            }
+        }
+
+
+    }
 
 }
+
+
 
--- src/test/kotlin/Stud.kt
+++ src/test/kotlin/Stud.kt
@@ -106,4 +106,16 @@ class Stud {
         assertEquals(num9, 9)
     }
 
+    //@Test
+    fun dummyTest() {
+        while (true) {
+            for (i in 1..10) {
+                println(i)
+                if (i == 5) {
+                    break
+                }
+            }
+        }
+    }
+
 }
